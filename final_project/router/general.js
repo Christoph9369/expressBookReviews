1f9db63d7219e41ebd175a9bc7254a46ae5a0d51
Christoph9369 <mojuyefelix@gmail.com>
2024-10-17 20:47:53 -0400

    getting the book details based on the title

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -25,13 +25,41 @@ public_users.get('/isbn/:isbn',function (req, res) {
   
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
+    // Extract the author parameter from the request URL
+    const author = req.params.author.toLowerCase();
+
+    // Convert the 'books' object to an array using Object.values()
+    let booksArray = Object.values(books);
+
+    // Use the filter method to find books that match the author
+    let filteredBooks = booksArray.filter((book) => book.author.toLowerCase() === author);
+
+    // Check if any books were found by the given author
+    if (filteredBooks.length > 0) {
+        res.send(filteredBooks);
+    } else {
+        res.status(404).json({ message: "Author not found" });
+    }
   
 });
 
 // Get all books based on title
 public_users.get('/title/:title',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  // Extract the author parameter from the request URL
+  const title = req.params.title.toLowerCase();
+
+  // Convert the 'books' object to an array using Object.values()
+  let booksArray = Object.values(books);
+
+  // Use the filter method to find books that match the title
+  let filteredBooks = booksArray.filter((book) => book.title.toLowerCase() === title);
+
+  // Check if any books were found by the given title
+  if (filteredBooks.length > 0) {
+      res.send(filteredBooks);
+  } else {
+      res.status(404).json({ message: "title not found" });
+  }
 });
 
 //  Get book review
