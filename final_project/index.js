f9babbff4225f5a72998987a603e67f6eb480f0f
Christoph9369 <mojuyefelix@gmail.com>
2024-10-17 20:12:18 -0400

    getting the book details based on ISBN under

--- final_project/index.js
+++ final_project/index.js
@@ -10,8 +10,26 @@ app.use(express.json());
 
 app.use("/customer",session({secret:"fingerprint_customer",resave: true, saveUninitialized: true}))
 
+//Middleware for user
+
 app.use("/customer/auth/*", function auth(req,res,next){
-//Write the authenication mechanism here
+    //checck if user is authenticated
+    if(req.session.authorization){
+        let token = req.session.authorization['accessToken'] // Access Token
+
+        //Verify JWT token for user authentication
+        jwt.verify(token, "access", (err, user) => {
+            if (!err) {
+                req.user = user; // Set authenticated user data on the request object
+                next() //proceed to the next midlleware
+            } else { 
+                return res.status(403).json({message: "User not authenticated"}) // Return error if the token verification fails
+            }
+        })
+    } else {
+        return res.status(403).json({message: "User not authenticated"})
+    }
+
 });
  
 const PORT =5000;
