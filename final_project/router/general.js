ed82caf9ff9b9f28c25f2905e3f2f331a85face7
Christoph9369 <mojuyefelix@gmail.com>
2024-10-17 22:53:47 -0400

    logging in as a registered user

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -6,8 +6,26 @@ const public_users = express.Router();
 
 
 public_users.post("/register", (req,res) => {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  // Extract username and password from the request body
+  const { username, password } = req.body;
+
+  // Check if both username and password are provided
+  if (!username || !password) {
+      return res.status(400).json({ message: "Username and password are required" });
+  }
+
+  // Check if the username already exists in the users array
+  const userExists = users.some((user) => user.username === username);
+
+  if (userExists) {
+      return res.status(409).json({ message: "Username already exists" });
+  }
+
+  // If username does not, create a new user and add to the users array
+  users.push({ "username" : username, "password" : password });
+
+  // Return success message
+  return res.status(200).json({ message: "User successfully registered" });
 });
 
 // Get the book list available in the shop
@@ -45,7 +63,7 @@ public_users.get('/author/:author',function (req, res) {
 
 // Get all books based on title
 public_users.get('/title/:title',function (req, res) {
-  // Extract the author parameter from the request URL
+  // Extract the title parameter from the request URL
   const title = req.params.title.toLowerCase();
 
   // Convert the 'books' object to an array using Object.values()
@@ -64,8 +82,20 @@ public_users.get('/title/:title',function (req, res) {
 
 //  Get book review
 public_users.get('/review/:isbn',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  // Extract the ISBN parameter from the request URL
+  const isbn = req.params.isbn;
+
+  // Check if the book with the given ISBN exists
+  if (books[isbn]) {
+      // Get the reviews of the book
+      const bookReviews = books[isbn].reviews;
+
+      // Return the reviews
+      res.send(bookReviews);
+  } else {
+      // If the book with the given ISBN doesn't exist, return a 404 error
+      res.status(404).json({ message: "Book not found" });
+  }
 });
 
 module.exports.general = public_users;
